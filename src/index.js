bb174998d8b14254f7a82eac24da88a6eb3e63bd
Kunal <kunal34255@gmail.com>
2026-06-18 13:27:05 +0530

    refactor(client): remove GuildMembers intent from client setup

--- src/index.js
+++ src/index.js
@@ -50,7 +50,6 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
   ],
 });
 
